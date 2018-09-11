c88c449c01f2d230eeb5383d75cc8863739ff83d
Ivan Martinez C <vanmartc@gmail.com>
2018-09-10 19:29:20 -0500

    enabling rails admins dasboard with user authentication

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'cancancan'
 #User Roles
 gem 'rolify'
 
+#Admin dashboard
+gem 'rails_admin', '~> 1.3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  #define route for dasboard
+  mount RailsAdmin::Engine => '/admin_dashboard', as: 'rails_admin'
+  
+  #user authentication routes
   devise_for :users
-  root to: 'home#landing' 
 
+  #define the main route
+  root to: 'home#landing' 
+  
+  #define welcome route
   get 'welcome', to: 'home#welcome', as: :onlyAdmin
 end
