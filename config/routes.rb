348757074482cc7be1d61aac30584621e3a5a9d0
vanmartc <vanmartc@gmail.com>
2018-09-12 21:28:55 +0000

    Revert "Admin dasboard"

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  #define route for dasboard
-  mount RailsAdmin::Engine => '/admin_dashboard', as: 'rails_admin'
-  
-  #user authentication routes
   devise_for :users
+  root to: 'home#landing' 
 
-  #define the main routes 
-  authenticated :user do
-    root to: 'home#welcome'
-  end
-  unauthenticated :user do
-    root to: 'home#landing'
-  end
-
-  #define welcome route
-  get 'welcome', to: 'home#welcome', as: :welcome
-  get 'landing', to: 'home#landing', as: :landing
+  get 'welcome', to: 'home#welcome', as: :onlyAdmin
 end
